cc0136960a6d38a16d9f7f64ce4bb1bede522a44
jeremystrom <git@strommedia.com>
2014-03-27 15:42:16 -0500

    Updated for old (non-Universal) analytics.

--- js/gf-analytics.js
+++ js/gf-analytics.js
@@ -1,8 +1,10 @@
 (function ($) {
   $(document).on('gform_confirmation_loaded', function (event, form_id) {
+    var theLabel = window.gf_event_form_labels[form_id];
     if(typeof ga == 'function') {
-      var theLabel = window.gf_event_form_labels[form_id];
       ga("send", "event", "Forms", "Submission", theLabel);
+    } else  if(typeof _gaq == 'object') {
+      _gaq.push(['_trackEvent', 'Forms', 'Submission', theLabel]);
     }
   });
 })(jQuery);
